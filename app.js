0b22eb24a740d0c2e6885e6175673105e847dda3
fora-heims <foraheims@gmail.com>
2021-09-30 14:38:43 -0700

    Add function has been connected to an event listener and DOM elements and is functionally outputting the correct addition answer

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-// import functions and grab DOM elements
+// import functions 
+import { add } from './calculations.js';
+// grab DOM elements
 const input1 = document.getElementById('input-1');
-const calcSelect = document.getElementById('calc-select')
+const calcSelect = document.getElementById('calc-select');
 const input2 = document.getElementById('input-2');
-const button = document.getElementById('button');
-const input1 = document.getElementById('calculation-answer');
+const buttonCalc = document.getElementById('button-calc');
+const answer = document.getElementById('calculation-answer');
 
 // initialize global state
 
+
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+buttonCalc.addEventListener('click', ()=>{
+    // get user input
+    const value1 = Number(input1.value);
+    const value2 = Number(input2.value);
+    // use user input to update state 
+    const result = add(value1, value2);
+    // update DOM to reflect the new state
+    answer.textContent = result;
+    console.log(result);
+});
