08b198b97c06b25e2d84cb003500dbbb51869d38
fora-heims <foraheims@gmail.com>
2021-09-30 15:49:43 -0700

    Working multiply feature

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // import functions 
-import { add, subtract } from './calculations.js';
+import { add, subtract, multiply } from './calculations.js';
 // grab DOM elements
 const input1 = document.getElementById('input-1');
 const calcSelect = document.getElementById('calc-select');
@@ -18,12 +18,15 @@ buttonCalc.addEventListener('click', ()=>{
         // use user input to update state 
     const addResult = add(value1, value2);
     const subtractResult = subtract(value1, value2);
+    const multiplyResult = multiply(value1, value2);
     // update DOM to reflect the new state
     if (calcSelect.value === 'add') {
         answer.textContent = addResult;
     } else if (calcSelect.value === 'subtract') {
         answer.textContent = subtractResult;
-    } else {
+    } else if (calcSelect.value === 'multiply') {
+      answer.textContent = multiplyResult;
+  } else {
       answer.textContent = 'lol';
     }
 });
