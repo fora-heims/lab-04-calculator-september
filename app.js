c139e5b127ea33c66b3a2cc3a21d74b4a2bc40d6
fora-heims <foraheims@gmail.com>
2021-09-30 14:11:10 -0700

    Style was added to index.css for the calculator buttons and inputs. DOM elements grabbed in app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // import functions and grab DOM elements
+const input1 = document.getElementById('input-1');
+const calcSelect = document.getElementById('calc-select')
+const input2 = document.getElementById('input-2');
+const button = document.getElementById('button');
+const input1 = document.getElementById('calculation-answer');
 
 // initialize global state
 
