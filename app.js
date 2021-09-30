8068db21029385a9653ac31a99111a786d8fc207
fora-heims <foraheims@gmail.com>
2021-09-30 15:35:28 -0700

    subtract function integrated into the calculator using selector and else if conditional statements

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // import functions 
-import { add } from './calculations.js';
+import { add, subtract } from './calculations.js';
 // grab DOM elements
 const input1 = document.getElementById('input-1');
 const calcSelect = document.getElementById('calc-select');
@@ -15,9 +15,15 @@ buttonCalc.addEventListener('click', ()=>{
     // get user input
     const value1 = Number(input1.value);
     const value2 = Number(input2.value);
-    // use user input to update state 
-    const result = add(value1, value2);
+        // use user input to update state 
+    const addResult = add(value1, value2);
+    const subtractResult = subtract(value1, value2);
     // update DOM to reflect the new state
-    answer.textContent = result;
-    console.log(result);
+    if (calcSelect.value === 'add') {
+        answer.textContent = addResult;
+    } else if (calcSelect.value === 'subtract') {
+        answer.textContent = subtractResult;
+    } else {
+      answer.textContent = 'lol';
+    }
 });
