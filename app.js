f7bff0b99785d9a325f2c5ea98dad777bd277033
fora-heims <foraheims@gmail.com>
2021-09-30 20:49:15 -0700

    Small style edits and added 'press Enter' feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import functions 
 import { add, subtract, multiply, divide, modulo, intDiv } from './calculations.js';
+
 // grab DOM elements
 const input1 = document.getElementById('input-1');
 const calcSelect = document.getElementById('calc-select');
@@ -7,22 +8,19 @@ const input2 = document.getElementById('input-2');
 const buttonCalc = document.getElementById('button-calc');
 const answer = document.getElementById('calculation-answer');
 
+
 // initialize global state
 
-
 // set event listeners 
-buttonCalc.addEventListener('click', ()=>{
-    // get user input
+buttonCalc.addEventListener('click', ()=> {
     const value1 = Number(input1.value);
     const value2 = Number(input2.value);
-        // use user input to update state 
     const addResult = add(value1, value2);
     const subtractResult = subtract(value1, value2);
     const multiplyResult = multiply(value1, value2);
     const divideResult = divide(value1, value2);
     const moduloResult = modulo(value1, value2);
     const intDivResult = intDiv(value1, value2);
-    // update DOM to reflect the new state
     if (calcSelect.value === 'add') {
         answer.textContent = addResult;
     } else if (calcSelect.value === 'subtract') {
@@ -37,5 +35,33 @@ buttonCalc.addEventListener('click', ()=>{
         answer.textContent = intDivResult;
     } else {
         answer.textContent = 'lol';
+    }
+});
+
+window.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+        const value1 = Number(input1.value);
+        const value2 = Number(input2.value);
+        const addResult = add(value1, value2);
+        const subtractResult = subtract(value1, value2);
+        const multiplyResult = multiply(value1, value2);
+        const divideResult = divide(value1, value2);
+        const moduloResult = modulo(value1, value2);
+        const intDivResult = intDiv(value1, value2);
+        if (calcSelect.value === 'add') {
+            answer.textContent = addResult;
+        } else if (calcSelect.value === 'subtract') {
+            answer.textContent = subtractResult;
+        } else if (calcSelect.value === 'multiply') {
+            answer.textContent = multiplyResult;
+        } else if (calcSelect.value === 'divide') {
+            answer.textContent = divideResult;
+        } else if (calcSelect.value === 'modulo') {
+            answer.textContent = moduloResult;
+        } else if (calcSelect.value === 'int-div') {
+            answer.textContent = intDivResult;
+        } else {
+            answer.textContent = 'lol';
+        }
     }
 });
